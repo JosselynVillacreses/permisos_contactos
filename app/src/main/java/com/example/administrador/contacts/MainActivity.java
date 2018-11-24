01393e7ff2372610c39d8449442a0d18b0f8fcc3
erickatoscanog <erickatoscano.95@gmail.com>
2018-11-24 10:42:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/administrador/contacts/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.administrador.contacts;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button;
+    TextView textView;
+    public  static final int RequestPermissionCode  = 1 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.btn);
+        textView=(TextView)findViewById(R.id.contacts);
+
+        EnableRuntimePermission();
+        button.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn:
+                obtenerDatos();
+
+
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+    private void obtenerDatos() {
+
+
+        String[] projeccion = new String[] { ContactsContract.Data._ID, ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE };
+        String selectionClause = ContactsContract.Data.MIMETYPE + "='" +
+                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "' AND "
+                + ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL";
+        String sortOrder = ContactsContract.Data.DISPLAY_NAME + " ASC";
+
+        Cursor c = getContentResolver().query(
+                ContactsContract.Data.CONTENT_URI,
+                projeccion,
+                selectionClause,
+                null,
+                sortOrder);
+
+        textView.setText("");
+
+
+        while(c.moveToNext()){
+            textView.append(" Nombre: " + c.getString(1) + " Número: " + c.getString(2) +"\n");
+        }
+        c.close();
+
+        /*contactsCursor = getContentResolver().query(
+                ContactsContract.Contacts.CONTENT_URI,   // URI de contenido para los contactos
+                projection,                        // Columnas a seleccionar
+                selectionClause                    // Condición del WHERE
+                selectionArgs,                     // Valores de la condición
+                sortOrder);                        // ORDER BY columna [ASC|DESC]*/
+
+    }
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this,
+                Manifest.permission.READ_CONTACTS))
+        {
+
+            Toast.makeText(MainActivity.this,"CONTACTS permission allows us to Access CONTACTS app", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(MainActivity.this,"ACCESS CONTACTS", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(MainActivity.this,"Permission Canceled, Now your application cannot access CONTACTS.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+}
